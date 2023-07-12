2adf9993c001b01adfa6da2b9dfd306a37f65aa7
Arun <39628450+joviyal-arun@users.noreply.github.com>
2023-07-12 06:42:44 +0530

    Add files via upload

--- LOAN_PREDICTION_CODE/app.py
+++ LOAN_PREDICTION_CODE/app.py
@@ -24,15 +24,45 @@ def completed():
 
         new_gender=request.form.get('Gender')
         new_married_sts=request.form.get('Married')
-        dependent_count=int(request.form.get('Dependents'))
+
+        dependent_count=(request.form.get('Dependents'))
+        if dependent_count=='0':
+            dependent_count_name='Not applicable'
+        else:
+            dependent_count_name=dependent_count
+        
         education_sts=request.form.get('Education')
+        if education_sts=='1':
+            education_sts_name='Graduate'
+        else:
+            education_sts_name='Not graduate'
+
         self_emp_sts=request.form.get('Self_Employed')
+        if self_emp_sts=='0':
+            self_emp_sts_name='Not self employed'
+        else:
+            self_emp_sts_name='self employed'
+
         applicant=int(request.form.get('ApplicantIncome'))
         co_applicant=int(request.form.get('CoapplicantIncome'))
         loan_amount=int(request.form.get('LoanAmount'))
         loan_amount_term=int(request.form.get('Loan_Amount_Term'))
         credit=int(request.form.get('Credit_History'))
+
+        if credit==1:
+            credit_name='Yes'
+        else:
+            credit_name='No'
+
         property_area=request.form.get('Property_Area')
+
+        if property_area=='0':
+            property_area_name='Semiurban'
+        
+        elif property_area=='1':
+            property_area_name='urban'
+        else:
+            property_area_name='rural'
 
         new_gender_transform=label_encoder.fit_transform([new_gender])
         new_married_sts_transform=label_encoder.fit_transform([new_married_sts])
@@ -56,7 +86,7 @@ def completed():
         else:
             output='Loan Approved'
 
-    return render_template('status.html',Gender=new_gender,Married=new_married_sts,Dependents=dependent_count,Education=education_sts,Self_Employed=self_emp_sts,ApplicantIncome=applicant,CoapplicantIncome=co_applicant,LoanAmount=loan_amount,Loan_Amount_Term=loan_amount_term,Credit_History=credit,Property_Area=property_area,status=output)
+    return render_template('status.html',Gender=new_gender,Married=new_married_sts,Dependents=dependent_count_name,Education=education_sts_name,Self_Employed=self_emp_sts_name,ApplicantIncome=applicant,CoapplicantIncome=co_applicant,LoanAmount=loan_amount,Loan_Amount_Term=loan_amount_term,Credit_History=credit_name,Property_Area=property_area_name,status=output)
 
 
         
